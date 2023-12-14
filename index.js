79cc793a5528447e4ac1fff62cbbfba6c7d12f9e
DESKTOP-JMPL0N5\Jeffh <Jeffh934@gmail.com>
2023-12-13 20:17:36 -0500

    some basic setup. Adding discord.js import, adding node modules, adding ESLint.

---
+++ index.js
@@ -0,0 +1,16 @@
+// Discord.js version ^13.0 requires us to explicitly define client intents
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once).
+// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
+// It makes some properties non-nullable.
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+// Log in to discord with your client's token
+client.login(token);
